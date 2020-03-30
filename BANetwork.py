96e40177578c01b90a95f5ca894e65ac0425f4fc
shawn <shawntan72@gmail.com>
2020-03-30 10:13:36 +0800

    Completed project

--- BANetwork.py
+++ BANetwork.py
@@ -124,3 +124,183 @@ class Network:
     def count_deg_freq(self):
         self.deg_freq = dict(Counter(self.degree))
         return self
+    
+
+class NetworkRW:
+    def __init__(self, m, q, N=False):
+        '''Creates a graph represented by a list of N lists where each
+        inner list shows all neighbours connected to that vertex. Initialises
+        with each vertex having 1 edge.
+        Parameters
+        ----------
+        N : int
+            Size of graph to initialise
+        m : int
+            Number of edges to add with each new vertex
+        q : float
+            Probability of continuing a random walk after each step
+        '''
+        if N is False:
+            self.N = m + 1
+        else:
+            self.N = N
+        self.m = m
+        self.q = q
+        self.random = []
+        self.graph = self.initialise_graph()
+        self.vertices = None
+        self.degree = None
+        self.deg_freq = None
+        
+    def initialise_graph(self):
+        vertices = [n for n in range(self.N)]
+        graph = {}
+        for vertex in vertices:
+            graph[vertex] = [v for v in vertices if v!=vertex]
+        return graph
+    
+    def add_vertex(self, neighbours):
+        '''Adds a vertex with given neighbours and update neighbours.'''
+        for n in neighbours:
+            self.graph[n].append(self.N)
+        self.graph[self.N] = neighbours
+        return self
+    
+    def gen_random(self):
+        self.random = np.random.random_sample(2 * self.m / self.q)
+    
+    def gen_neighbours(self, m):
+        '''Generates a list of m neighbours from vertex list.'''
+        vertex_list = list(self.graph)
+        neighbours = list(set(random.sample(vertex_list, m)))
+        rand = np.random.rand
+        print('walking for: {}'.format(str(neighbours)))
+        for i in range(len(neighbours)):
+            print('  start:' + str(neighbours[i]))
+            while rand() < self.q:
+                neighbours[i] = random.choice(self.graph[neighbours[i]])
+                print('    to:' + str(neighbours[i]))
+            print('  end:{}'.format(str(neighbours[i])))
+        print('end state:{}'.format(str(neighbours)))
+        return neighbours
+    
+    def gen_neighbour(self):
+        '''Generates a new neighbour from random walk'''
+        rand = np.random.rand()
+        neighbour = random.choice(list(self.graph))
+        while rand < self.q:
+            neighbour = random.choice(self.graph[neighbour])
+            rand = np.random.rand()
+        return neighbour
+    
+    def add_vertices(self, N):
+        '''Adds vertices until graph has N number of vertices'''
+        if N < self.N:
+            print('Given N is smaller than number of vertices')
+        while self.N < int(N):
+            neighbours = []
+            while len(neighbours) < self.m:
+                neighbour = self.gen_neighbour()
+                if neighbour not in neighbours:
+                    neighbours.append(neighbour)
+            self.add_vertex(neighbours)
+            self.N += 1
+        return self
+
+    def count_deg(self):
+        '''Count the degree for each vertex.'''
+        degree = {}
+        for vertex in self.graph:
+            degree[vertex] = len(self.graph[vertex])
+        self.degree = degree
+        return self
+
+class NetworkRW:
+    def __init__(self, m, q, N=False):
+        '''Creates a graph represented by a list of N lists where each
+        inner list shows all neighbours connected to that vertex. Initialises
+        with each vertex having 1 edge.
+        Parameters
+        ----------
+        N : int
+            Size of graph to initialise
+        m : int
+            Number of edges to add with each new vertex
+        prob : str
+            Probability model to use. 'preferential' or 'random'
+        '''
+        prob_models = ['preferential', 'random']
+        if N is False:
+            self.N = m + 1
+        else:
+            self.N = N
+        self.m = m
+        self.q = q
+        self.random = []
+        self.graph = self.initialise_graph()
+        self.vertices = None
+        self.degree = None
+        self.deg_freq = None
+        
+    def initialise_graph(self):
+        vertices = [n for n in range(self.N)]
+        graph = {}
+        for vertex in vertices:
+            graph[vertex] = [v for v in vertices if v!=vertex]
+        return graph
+    
+    def add_vertex(self, neighbours):
+        '''Adds a vertex with given neighbours and update neighbours.'''
+        for n in neighbours:
+            self.graph[n].append(self.N)
+        self.graph[self.N] = neighbours
+        return self
+    
+    def gen_random(self):
+        self.random = np.random.random_sample(2 * self.m / self.q)
+    
+    def gen_neighbours(self, m):
+        '''Generates a list of m neighbours from vertex list.'''
+        vertex_list = list(self.graph)
+        neighbours = list(set(random.sample(vertex_list, m)))
+        rand = np.random.rand
+        print('walking for: {}'.format(str(neighbours)))
+        for i in range(len(neighbours)):
+            print('  start:' + str(neighbours[i]))
+            while rand() < self.q:
+                neighbours[i] = random.choice(self.graph[neighbours[i]])
+                print('    to:' + str(neighbours[i]))
+            print('  end:{}'.format(str(neighbours[i])))
+        print('end state:{}'.format(str(neighbours)))
+        return neighbours
+    
+    def gen_neighbour(self):
+        '''Generates a new neighbour from random walk'''
+        rand = np.random.rand()
+        neighbour = random.choice(list(self.graph))
+        while rand < self.q:
+            neighbour = random.choice(self.graph[neighbour])
+            rand = np.random.rand()
+        return neighbour
+    
+    def add_vertices(self, N):
+        '''Adds vertices until graph has N number of vertices'''
+        if N < self.N:
+            print('Given N is smaller than number of vertices')
+        while self.N < int(N):
+            neighbours = []
+            while len(neighbours) < self.m:
+                neighbour = self.gen_neighbour()
+                if neighbour not in neighbours:
+                    neighbours.append(neighbour)
+            self.add_vertex(neighbours)
+            self.N += 1
+        return self
+
+    def count_deg(self):
+        '''Count the degree for each vertex.'''
+        degree = {}
+        for vertex in self.graph:
+            degree[vertex] = len(self.graph[vertex])
+        self.degree = degree
+        return self
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # BANetwork
 2020 Imperial College Networks Project
+
+All code is in a Jupyter Notebook that can be run sequentially. Data files are omitted. Networks object is in a separate python file that is imported into the notebook.
